80cc2270ac6563b56986b16e30947579787b0e45
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-28 23:55:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+  i = 0 
+  newArray = []
+  
+  while i < array.length 
+    yield (array[i])
+    i += 1 
+  end
+  for element in array
+    newArray.push yield element
+  end
+  newArray
+end
 
