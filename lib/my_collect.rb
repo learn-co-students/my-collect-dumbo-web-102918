b816af89e41fc0cb887154f1d7658bf8ad6fc5b7
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-16 19:59:43 +0000

    Done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+def my_collect(array)
+  if block_given?
+    i = 0
+    collection = []
+  end
+    
+    while i < array.length
+    collection << yield(array[i])
+    i += 1
+    end
+    collection
+end
 
+
+
+my_collect([]) do |x|
+  x
+end
+    
+    
+    
 
