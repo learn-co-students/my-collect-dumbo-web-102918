ceb560d5b921ca48f218f75d52d298f73344cb36
naomijoyce <naomijoyceb@gmail.com>
2018-10-22 19:19:47 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect (array)
+  i = 0
+  other_array = []
+  while i < array.count
+    this_thing = yield(array[i])
+    other_array << this_thing
+    i = i.next
+  end
+  return other_array
+end
